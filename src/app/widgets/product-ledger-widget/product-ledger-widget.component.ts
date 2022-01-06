a440696f755ac3c0e8cf5d7c33dfbee03e0f551c
Shreeyanshu kesherwani <shreeyanshukesherwani@gmail.com>
2022-01-06 13:44:18 +0530

    product-ledger-widgit

--- src/app/widgets/product-ledger-widget/product-ledger-widget.component.ts
+++ src/app/widgets/product-ledger-widget/product-ledger-widget.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ChartData, ChartOptions, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-product-ledger-widget',
@@ -8,10 +9,29 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ProductLedgerWidgetComponent implements OnInit {
   @Input() Bakery: string = "TIRUPATI BAKERS";
   @Input() ItemName: string = "Biscuit";
-  @Input() Safequantity:string = "98Kg";
-  @Input() Damagequantity:string = "28Kg";
-  @Input() Suppcode:string = "123XYZ0";
+  @Input() Safe:number = 0;
+  @Input() Damage:number = 0;
+  @Input() Ledgerno:string = "123XYZ0";
   @Input() Price:string = "23245";
+
+  public doughnutChartLabels: string[] = ['Safe', 'Damaged'];
+  public doughnutChartData: ChartData<'doughnut'> = {
+      labels: this.doughnutChartLabels,
+      datasets: [
+      { data: [this.Safe, this.Damage] },
+    ]
+  };
+  public doughnutChartType: ChartType = 'doughnut';
+  // events
+  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  
   constructor() { }
 
   ngOnInit() {}
